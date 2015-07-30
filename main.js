81d7a6e929b3715ebcee8f84dfa73897d84d5858
danielcossins11 <daniel.cossins@yahoo.com>
2015-07-30 10:25:00 -0500

    added pictures for each language

--- main.js
+++ main.js
@@ -13,16 +13,21 @@ var Language = function(){
   };
 }();
 
+var $flag = $('body');
+
 $('button').on("click", function(){
   var $select = $('select').val();
   if($select==="Latin"){
-   Language.translateToLatin();
+    Language.translateToLatin();
+    $flag.css("background", "url('https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/She-wolf_suckles_Romulus_and_Remus.jpg/310px-She-wolf_suckles_Romulus_and_Remus.jpg') no-repeat center center fixed");
   }
   if($select==="German"){
-   Language.translateToGerman(); 
+    Language.translateToGerman();
+    $flag.css("background", "url('http://www.maxbratwurst.com/images/german_guy.png') no-repeat center center fixed");
   }
   if($select==="French"){
-   Language.translateToFrench();
+    Language.translateToFrench();
+    $flag.css("background", "url('http://images.fineartamerica.com/images-medium-large-5/french-fries-budi-satria-kwan.jpg') no-repeat center center fixed");
   }
   
 })
